b355ba79e0bfe50f0df2cb58e5b64d04e5561299
isnotafunction <ivana.cosgrove@gmail.com>
2018-02-02 15:05:06 +0000

    remove buttons work in both to do and done, localStorage updates accordingly

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var newItem = document.querySelector(".newItem");
 var submit = document.querySelector(".submit");
 var toDoList = document.querySelector(".toDoList");
+var doneList = document.querySelector(".doneList");
 var p = document.querySelector("p");
 var li = document.getElementsByTagName("li");
 
-//renderHTML from localStorage
+//renderHTML from localStorage if there is anything
 var data = (localStorage.getItem("toDo")) ? JSON.parse(localStorage.getItem("toDo")): {
   todo : [],
+  completed:[]
 };
 console.log(data)
 renderToDoList()
@@ -16,9 +18,9 @@ submit.addEventListener("click", function(){
   var value = newItem.value;
   var item =
   `<li>
-  <button id="tick" class="buttons" onclick="turnOn(this)"><div class="circle green"></div></button>
+  <button id="tick" class="buttons" onClick="turnOn(this)"><div class="circle green"></div></button>
   <span>${value}</span>
-  <button id="x" onClick="remove(this)" class="buttons"><div class="circle red"></div></button>
+  <button id="x" onclick="remove(this)" class="buttons"><div class="circle red"></div></button>
   </li>`
   if(value){
     toDoList.insertAdjacentHTML('afterbegin', item)
@@ -28,19 +30,38 @@ submit.addEventListener("click", function(){
     dataUpdated()
 })
 
-  //tick off list item
+  //complete list item
   function turnOn(el){
-    console.log(this)
-    el.classList.toggle("on")
-    el.parentNode.classList.toggle("done")
+    var value = el.parentNode.childNodes[3].innerText;
+    data.completed.push(value)
+    data.todo.splice(data.todo.indexOf(value),1)
+    el.parentNode.parentNode.removeChild(el.parentNode);
+    var item =
+    `<li>
+    <button id="tick" class="buttons on"><div class="circle green"></div></button>
+    <span>${value}</span>
+    <button id="x" onclick="removeCompleted(this)" class="buttons"><div class="circle red"></div></button>
+    </li>`
+    doneList.insertAdjacentHTML('afterbegin', item)
+    dataUpdated()
   }
 
  //remove item list and remove from array- very hacky, sort your html out!
   function remove(el){
+   alert("clicked remove");
    var text = el.parentNode.childNodes[3].innerText
    data.todo.splice(data.todo.indexOf(text),1)
    el.parentNode.parentNode.removeChild(el.parentNode);
    dataUpdated()
+  }
+
+  //remove item from done list
+  function removeCompleted(el){
+    alert("clicked clear");
+    el.parentNode.parentNode.removeChild(el.parentNode);
+    var value = el.parentNode.childNodes[3].innerText;
+    data.completed.splice(data.completed.indexOf(value),1)
+    dataUpdated()
   }
 
  //storing in localStorage
@@ -50,15 +71,27 @@ submit.addEventListener("click", function(){
 
   //render html
   function renderToDoList(){
-    if(!data.todo.length) return;
+    if(!data.todo.length && !data.completed.length) return;
     for(var i=0; i<data.todo.length; i++){
      var value = data.todo[i];
      var item =
      `<li>
      <button id="tick" class="buttons" onclick="turnOn(this)"><div class="circle green"></div></button>
      <span>${value}</span>
-     <button id="x" onClick="remove(this)" class="buttons"><div class="circle red"></div></button>
+     <button id="x" onclick="remove(this)" class="buttons"><div class="circle red"></div></button>
      </li>`
      toDoList.insertAdjacentHTML('afterbegin', item)
+    }
+
+   for(var j=0; j<data.completed.length; j++){
+      var value = data.completed[j];
+      console.log("this is completed of i "+ value)
+      var item =
+      `<li>
+      <button id="tick" class="buttons on"><div class="circle green"></div></button>
+      <span>${value}</span>
+      <button id="x" onclick="clear(this)" class="buttons"><div class="circle red"></div></button>
+      </li>`
+      doneList.insertAdjacentHTML('afterbegin', item)
     }
   }
