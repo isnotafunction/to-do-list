7f8cd2144c5de8f5c32e215173c1358a187f4f71
isnotafunction <ivana.cosgrove@gmail.com>
2018-02-02 12:18:24 +0000

    adds to localStorage and renders HTML from localStorage

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
 var newItem = document.querySelector(".newItem");
 var submit = document.querySelector(".submit");
-var toDoList = document.querySelector(".toDoList")
-var completed = document.querySelectorAll("#tick");
-console.log(completed)
+var toDoList = document.querySelector(".toDoList");
+var p = document.querySelector("p");
+var li = document.getElementsByTagName("li");
 
+//renderHTML from localStorage
+var data = (localStorage.getItem("toDo")) ? JSON.parse(localStorage.getItem("toDo")): {
+  todo : [],
+};
+console.log(data)
+renderToDoList()
+
+//add new item
 submit.addEventListener("click", function(){
   var value = newItem.value;
   var item =
-  `<li type="checkbox">
-  ${value}
-  <button onclick="turnOn(this)" id="tick" class="buttons"><div class="check"></div></button>
-  <button onClick="this.parentNode.parentNode.removeChild(this.parentNode);" id="x"class="buttons">x</button>
+  `<li>
+  <button id="tick" class="buttons" onclick="turnOn(this)"><div class="circle green"></div></button>
+  <span>${value}</span>
+  <button id="x" onClick="remove(this)" class="buttons"><div class="circle red"></div></button>
   </li>`
-
   if(value){
     toDoList.insertAdjacentHTML('afterbegin', item)
-  }
-  newItem.value="";
+    data.todo.push(value);
+    }
+    newItem.value="";
+    dataUpdated()
 })
 
-function turnOn(el){
-  el.classList.toggle("on")
-}
+  //tick off list item
+  function turnOn(el){
+    console.log(this)
+    el.classList.toggle("on")
+    el.parentNode.classList.toggle("done")
+  }
+
+ //remove item list and remove from array- very hacky, sort your html out!
+  function remove(el){
+   var text = el.parentNode.childNodes[3].innerText
+   data.todo.splice(data.todo.indexOf(text),1)
+   el.parentNode.parentNode.removeChild(el.parentNode);
+   dataUpdated()
+  }
+
+ //storing in localStorage
+  function dataUpdated(){
+    localStorage.setItem("toDo", JSON.stringify(data))
+  }
+
+  //render html
+  function renderToDoList(){
+    if(!data.todo.length) return;
+    for(var i=0; i<data.todo.length; i++){
+     var value = data.todo[i];
+     var item =
+     `<li>
+     <button id="tick" class="buttons" onclick="turnOn(this)"><div class="circle green"></div></button>
+     <span>${value}</span>
+     <button id="x" onClick="remove(this)" class="buttons"><div class="circle red"></div></button>
+     </li>`
+     toDoList.insertAdjacentHTML('afterbegin', item)
+    }
+  }
